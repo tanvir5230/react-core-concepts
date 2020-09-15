008eda868f667aa4921b599814b71bf38104e83c
Tanvir Ibn Touhid <tanvirbintouhid5230@gmail.com>
2020-09-15 21:23:17 +0600

    useState and useEffects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
+  function increase() {
+    setCount(count + 1);
+  }
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>count: {count}</p>
+      <button
+        style={{
+          padding: "10px",
+          cursor: "pointer",
+          border: "none",
+          outline: "none",
+        }}
+        onClick={increase}
+      >
+        +
+      </button>
+      <ul>
+        {users.length === 0 && <p>loading...</p>}
+        {users.length > 0 &&
+          users.map((user) => {
+            return <li key={user.id}>{user.name}</li>;
+          })}
+      </ul>
     </div>
   );
 }
